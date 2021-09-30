c255b135a46b05d9e9a2c8d98a4c4f9e919434df
Elias de Abreu <elias.silva@ifro.edu.br>
2021-09-30 13:29:05 -0300

    Ordenação dos adjacentes

--- a_star_rotas.py
+++ a_star_rotas.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 #Classe para criar as vértices (nós) do grafo
 class Vertice:
@@ -20,7 +21,8 @@ class Vertice:
 class Adjacente:
     def __init__(self,vertice,custo):
         self.vertice = vertice
-        self.custo = custo
+        self.custo = custo # O custo do vértice até seu adjacente recebido por parâmetro
+        self.distancia_a_star = vertice.distancia_objetivo + self.custo
 
 #Classe utilizada para iniciar o grafo, criando as cidades e conectado-as
 class Grafo:
@@ -28,9 +30,9 @@ class Grafo:
     #Colocar o nome da cidade e o custo estimado até a cidade objetivo
     #Ver outras heurísticas
     #A primeira heurística é o tempo estimado para chegada, coloquei em minutos
-    #Os testes até então vão de Pimenta Bueno a Ouro Preto
+    # Os testes até então vão de Pimenta Bueno a Ouro Preto
     jipa = Vertice('Ji-paraná',0)
-    medice = Vertice('Presidente Médici',36)
+    medice = Vertice('Presidente Médici',36) # Nome da cidade e o f(h), heuristica, aqui a estimativa de tempo até o objetivo
     opo = Vertice('Ouro Preto do Oeste',45)
     teixeiropolis = Vertice('Teixeirópolis',55)
     alvorada = Vertice('Alvorada D. Oeste',150)
@@ -56,6 +58,7 @@ class Grafo:
     #jaru = Vertice('Jaru',95) #retirei Jaru para ficar marcado no grafo
     nova_uniao = Vertice('Nova União',90)
     mirante_da_serra = Vertice('Mirante da Serra',110)
+    alto_alegre_do_parecis = Vertice('Alto Alegre dos Parecis',160)
 
     #Conectar as cidades adjacentes com o custo estimado (inicialmente a distância)
 
@@ -64,7 +67,7 @@ class Grafo:
     jipa.add_adjacentes(Adjacente(teixeiropolis,53))
     jipa.add_adjacentes(Adjacente(alvorada,80))
     jipa.add_adjacentes(Adjacente(medice,36))
-    jipa.add_adjacentes(Adjacente(ministro_andreazza,127))
+    jipa.add_adjacentes(Adjacente(ministro_andreazza,88))
 
     #adicionar os adjacentes de Ouro Preto
     opo.add_adjacentes(Adjacente(jipa,40))
@@ -113,5 +116,119 @@ class Grafo:
     # Adicionar adjacentes de costa marques
     costa_marques.add_adjacentes(Adjacente(sao_francisco,110))
 
+    #Adicionar adjacentes de Nova Brasilândia
+    nova_brasilandia.add_adjacentes(Adjacente(sao_miguel,51))
+    nova_brasilandia.add_adjacentes(Adjacente(novo_horizonte,38))
+
+    #Adicionar adjacentes de Novo Horizonte do Oeste
+    novo_horizonte.add_adjacentes(Adjacente(nova_brasilandia,38))
+    novo_horizonte.add_adjacentes(Adjacente(rolim_de_moura,26))
+
+    medice.add_adjacentes(Adjacente(jipa,36))
+    medice.add_adjacentes(Adjacente(ministro_andreazza,90))
+    medice.add_adjacentes(Adjacente(castanheiras,41))
+    medice.add_adjacentes(Adjacente(cacoal,70))
+
+    ministro_andreazza.add_adjacentes(Adjacente(jipa,88))
+    ministro_andreazza.add_adjacentes(Adjacente(medice,90))
+    ministro_andreazza.add_adjacentes(Adjacente(cacoal,34))
+    ministro_andreazza.add_adjacentes(Adjacente(espigao_d_oeste,83))
+
+    cacoal.add_adjacentes(Adjacente(ministro_andreazza,34))
+    cacoal.add_adjacentes(Adjacente(medice,70))
+    cacoal.add_adjacentes(Adjacente(espigao_d_oeste,62))
+    cacoal.add_adjacentes(Adjacente(parecis,95))
+    cacoal.add_adjacentes(Adjacente(rolim_de_moura,63))
+    cacoal.add_adjacentes(Adjacente(pimenta_boeno,43))
+
+    parecis.add_adjacentes(Adjacente(cacoal,95))
+
+    espigao_d_oeste.add_adjacentes(Adjacente(ministro_andreazza,83))
+    espigao_d_oeste.add_adjacentes(Adjacente(cacoal,62))
+    espigao_d_oeste.add_adjacentes(Adjacente(pimenta_boeno,33))
+
+    #Adicionar adjacentes de Pimenta Bueno, faltou vilhena e Chupiguaia, pois até então não pretendemos usar
+    pimenta_boeno.add_adjacentes(Adjacente(cacoal,43))
+    pimenta_boeno.add_adjacentes(Adjacente(espigao_d_oeste,33))
+    pimenta_boeno.add_adjacentes(Adjacente(primavera_de_rondonia,28))
+    pimenta_boeno.add_adjacentes(Adjacente(rolim_de_moura,66))
+
+    rolim_de_moura.add_adjacentes(Adjacente(pimenta_boeno,66))
+    rolim_de_moura.add_adjacentes(Adjacente(cacoal,63))
+    rolim_de_moura.add_adjacentes(Adjacente(novo_horizonte,26))
+    rolim_de_moura.add_adjacentes(Adjacente(santa_luzia,20))
+
+    primavera_de_rondonia.add_adjacentes(Adjacente(pimenta_boeno,28))
+    primavera_de_rondonia.add_adjacentes(Adjacente(sao_felipe,30))
+
+    sao_felipe.add_adjacentes(Adjacente(primavera_de_rondonia,30))
+    sao_felipe.add_adjacentes(Adjacente(santa_luzia,29))
+
+    santa_luzia.add_adjacentes(Adjacente(sao_felipe,29))
+    santa_luzia.add_adjacentes(Adjacente(rolim_de_moura,20))
+    santa_luzia.add_adjacentes(Adjacente(alta_floresta,25))
+    santa_luzia.add_adjacentes(Adjacente(alto_alegre_do_parecis,33))
+
+    alto_alegre_do_parecis.add_adjacentes(Adjacente(santa_luzia,33))
+
+    alta_floresta.add_adjacentes(Adjacente(santa_luzia,25))
+
+#método responsável por ordenar os adjacentes
+class VetorOrdenado:
+
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        # Mudança no tipo de dados
+        self.valores = np.empty(self.capacidade, dtype=object)
+
+    # Referência para o vértice e comparação com a distância A*
+    def insere(self, adjacente):
+        if self.ultima_posicao == self.capacidade - 1:
+            print('Capacidade máxima atingida')
+            return
+        posicao = 0
+        for i in range(self.ultima_posicao + 1):
+            posicao = i
+            if self.valores[i].distancia_aestrela > adjacente.distancia_aestrela:
+                break
+            if i == self.ultima_posicao:
+                posicao = i + 1
+        x = self.ultima_posicao
+        while x >= posicao:
+            self.valores[x + 1] = self.valores[x]
+            x -= 1
+        self.valores[posicao] = adjacente
+        self.ultima_posicao += 1
+
+    def imprime(self):
+        if self.ultima_posicao == -1:
+            print('O vetor está vazio')
+        else:
+            for i in range(self.ultima_posicao + 1):
+                print(i, ' - ', self.valores[i].vertice.rotulo, ' - ',
+                      self.valores[i].custo, ' - ',
+                      self.valores[i].vertice.distancia_objetivo, ' - ',
+                      self.valores[i].distancia_aestrela)
+
+class Vetor_Ordenado:
+
+    def __init__(self,lista):
+        self.lista = lista
+
+    def ordenar_menor(self):
+        self.lista = sorted(self.lista, key=lambda a_star: a_star.distancia_a_star, reverse=False)
+
+    def imprimir_adjacentes_ordenados(self):
+        for i in self.lista:
+            print(i.vertice.rotulo, " Tempo estimado até Jipa (Minutos): ", i.vertice.distancia_objetivo,
+                  "  Distância (KM): ", i.custo, "  Distância A*: ", i.distancia_a_star)
+
+
 grafo = Grafo()
-grafo.costa_marques.show_adjacentes()
+#print(grafo.jipa.adjacentes[0].vertice.rotulo," Tempo estimado até Jipa (Minutos): ",grafo.jipa.adjacentes[0].vertice.distancia_objetivo,"  Distância (KM): ",grafo.jipa.adjacentes[0].custo, "  Distância A*: ",grafo.jipa.adjacentes[0].distancia_a_star)
+
+lista = grafo.jipa.adjacentes
+o = Vetor_Ordenado(lista)
+o.ordenar_menor()
+o.imprimir_adjacentes_ordenados()
